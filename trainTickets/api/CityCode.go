0ce57ec7a908d792282526dfd636bced23db6ada
mengmanzbh <1361006944@qq.com>
2019-01-09 17:40:55 +0800

    OK

--- trainTickets/api/CityCode.go
+++ trainTickets/api/CityCode.go
@@ -9,6 +9,16 @@ import (
     "encoding/json"
 )
 const APPKEY = "5b433b1f92d41bba340a5bb47464ce32" //您申请的APPKEY
+
+
+// @Summary 站点简码查询
+// @Description 站点简码查询
+// @Accept json
+// @Produce json
+// @Param stationName path string true "站点名，如苏州、苏州北，不需要加“站”字"
+// @Param all path string false "如果需要全部站点简码，请将此参数设为1"
+// @Router /trainTickets/cityCode [post]
+
 //站点简码查询
 func CityCode(ctx *gin.Context) {
 	stationName := ctx.PostForm("stationName")
