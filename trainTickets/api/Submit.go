3c485c98225b1fab96dc8e9eb5c2176b837a7d96
mengmanzbh <1361006944@qq.com>
2019-01-14 16:30:28 +0800

    OK

--- trainTickets/api/Submit.go
+++ trainTickets/api/Submit.go
@@ -57,10 +57,17 @@ func Submit(ctx *gin.Context) {
 	param.Set("passengers",passengers)//乘车人信息，每个订单最多5个乘客，json格式的字符串，如：[{"passengerid":1,"passengersename":"张三","piaotype":"1","piaotypename":"成人票","passporttypeseid":"1","passporttypeseidname":"二代身份证","passportseno":"420205199207231234","price":"763.5","zwcode":"M","zwname":"一等座"}]请想清楚，这里传json不代表所有的参数（如key、checi等）都是通过json传递过来！您测试的时候请将乘客信息换成真实信息，如使用非真实信息（张三、李四等）提交的订单过多，将会被禁止使用各个字段的解释：  "passengerid":乘客的顺序号，如：1，当一个订单有多个乘客时，用来唯一标识每个乘客，建议设为1-5（因每单最多5个乘客）的正整数；请自定义此参数。  "passengersename":乘车人姓名  "piaotype":如：1。其中，1 :成人票,2 :儿童票,4 :残军票  "piaotypename":如：成人票。票种名称，和上面的piaotype对应  "passporttypeseid": 如：1。其中，1:二代身份证,2:一代身份证,C:港澳通行证,B:护照,G:台湾通行证  "passporttypeseidname":如：二代身份证。证件类型名称，和上面的passporttypeseid对应  "passportseno":如：420205199207231234。乘客证件号码  "price": 票价，即当前乘客选择的座位的价格  "zwcode":如：1。表示座位编码，其中    F:动卧(新增),    9:商务座,    P:特等座,    M:一等座,    O（大写字母O，不是数字0）:二等座,    6:高级软卧,     4:软卧,    3:硬卧,    2:软座,    1:硬座。    注意:此处规则与12306不同，无座没有zwcode，当最低座位无票时,购买选该座位, 买下的就是无座。请务必阅读火车票订票接口常见问题中序号为7、8、9、9.1的内容  "zwname":如：硬座。表示座位名称，和上面的座位编码对应，注意：无座没有zwname，即不能由用户指定购买无座（无座仅作为最低座次的备选票）
     
     var juheorderid string
+    var error_code string
+    var reason string
 	//发送请求
 	data,err:=utils.Post(juheURL,param)
 	if err!=nil{
 		fmt.Errorf("请求失败,错误信息:\r\n%v",err)
+
+	    juheorderid = netReturn["result"].(map[string]interface{})["orderid"].(string)
+	    error_code = netReturn["error_code"].(string)
+	    reason = netReturn["reason"].(string)
+
 		ctx.JSON(404, gin.H{
 			"error_code": "404",
 			"message": err,
@@ -70,6 +77,9 @@ func Submit(ctx *gin.Context) {
 		json.Unmarshal(data,&netReturn)
 	
 	    juheorderid = netReturn["result"].(map[string]interface{})["orderid"].(string)
+	    error_code = netReturn["error_code"].(string)
+	    reason = netReturn["reason"].(string)
+	    
 		ctx.JSON(200, gin.H{
 			"error_code": netReturn["error_code"],
 			"message": netReturn["reason"],
@@ -86,9 +96,9 @@ func Submit(ctx *gin.Context) {
         fmt.Println("open success")
 
 	    nowTimeStr := GetTime()
-	    stmt, err := db.Prepare("insert train_tickets_orders set realname=?,nickname=?,cellphone=?,customer_id=?,user_orderid=?")
+	    stmt, err := db.Prepare("insert train_tickets_orders set realname=?,nickname=?,cellphone=?,customer_id=?,user_orderid=?,train_date=?,is_accept_standing=?,choose_seats=?,from_station_code=?,to_station_code=?,checi=?,passengers=?,orderid=?,reason=?,error_code=?,update_time=?")
 	    CheckErr(err)
-	    res, err := stmt.Exec(realname, nickname, cellphone, customer_id, user_orderid)
+	    res, err := stmt.Exec(realname, nickname, cellphone, customer_id, user_orderid,train_date,is_accept_standing,choose_seats,from_station_code,to_station_code,checi,passengers,juheorderid,reason,error_code,nowTimeStr)
 	    CheckErr(err)
 	    id, err := res.LastInsertId()
 	    CheckErr(err)
