6fc5affd990638646437bca0a71d0902a5e49fec
mengmanzbh <1361006944@qq.com>
2019-01-14 15:11:56 +0800

    OK

--- trainTickets/utils/utils.go
+++ trainTickets/utils/utils.go
@@ -6,10 +6,70 @@ import (
     "net/http"
     "net/url"
     "io/ioutil"
+    "bytes"
     "fmt"
     "crypto/md5"
     "encoding/hex"
 )
+
+// 根据授权码获取用户信息
+func GetUserByAccess(access string,ctx *gin.Context)(x float64,y string,z string,q string,istoken bool) {
+
+  var islogin bool
+  data := make(map[string]interface{})
+    data["access"] = access
+    bytesData, err := json.Marshal(data)
+    if err != nil {
+        fmt.Println(err.Error() )
+        return
+    }
+    reader := bytes.NewReader(bytesData)
+    url := "http://47.52.47.73:8112/auth/auth/GetUserByAccess"
+    request, err := http.NewRequest("POST", url, reader)
+    if err != nil {
+        fmt.Println(err.Error())
+        return
+    }
+    request.Header.Set("Content-Type", "application/json;charset=UTF-8")
+    client := http.Client{}
+    resp, err := client.Do(request)
+    if err != nil {
+        fmt.Println(err.Error())
+        return
+    }
+    respBytes, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        fmt.Println(err.Error())
+        return
+    }
+    var customer_id float64
+    var realname string
+    var nickname string
+    var cellphone string
+
+    //返回结果
+    var netReturn map[string]interface{}
+    json.Unmarshal(respBytes,&netReturn)
+    if netReturn["isSuccess"]==true{
+        userdata := netReturn["data"]
+        islogin = true
+        // fmt.Printf("获取用户信息:\r\n%v",userdata)
+        customer_id = userdata.(map[string]interface{})["customer_id"].(float64)
+        realname = userdata.(map[string]interface{})["realname"].(string)
+        nickname = userdata.(map[string]interface{})["nickname"].(string)
+        cellphone = userdata.(map[string]interface{})["cellphone"].(string)
+        
+    }else{
+        fmt.Println("获取token失败")
+        islogin = false
+        ctx.JSON(404, gin.H{
+            "error_code": "404",
+            "message": "token失效，请重新登录",
+        })
+        
+    }
+    return customer_id,realname,nickname,cellphone,islogin
+}
 
 // 生成32位MD5
 func MD5(text string) string {
